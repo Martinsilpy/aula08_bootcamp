f375de7da5f94a8dda48ef90551c2c48ff4a54b5
Michel Martins <michel.martinss@yahoo.com.br>
2024-11-23 19:25:13 -0300

    comentando a pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,6 @@
 from etl import pipeline_calcular_kpi_de_vendas_consolidada
 
 pasta_argumento: str = 'data' # Define o diretório onde os dados estão armazenados
-formato_de_saida: list = ["csv"] # Define os formatos de saída desejados
+formato_de_saida: list = ["parquet"] # Define os formatos de saída desejados
 
 pipeline_calcular_kpi_de_vendas_consolidada(pasta_argumento, formato_de_saida) # Executa a função pipeline para calcular o KPI de vendas consolidadas
